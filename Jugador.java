853228d8972d81b60806d0c1038d986ad81c5c3c
JorgePrG97 <jorgeprg97@gmail.com>
2016-11-30 12:14:30 +0100

    Add files via upload

---
+++ Jugador.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ludoteca;
+
+/**
+ *
+ * @authors Jose Ignacio Diaz, Cristian Posada y Jorge Prieto.
+ */
+public class Jugador 
+{
+    private String nombre;
+    private double fichasTotales;
+    private int jugadasGanadasBJ, jugadasGanadas7, jugadasPerdidasBJ, jugadasPerdidas7, jugadasEmpatadas;
+    
+    public Jugador (String n)
+    {
+        nombre=n;
+        fichasTotales=100;
+    }
+    public Jugador (Jugador j)
+    {
+        nombre=j.nombre;
+        fichasTotales=j.fichasTotales;
+    }
+    
+    public String getNombre()
+    {
+        return nombre;
+    }
+    public void setNombre(String n)
+    {
+        nombre=n;
+    }
+    
+    public double getFichasTotales()
+    {
+        return fichasTotales;
+    }
+    public void setFichasTotales(double ft)
+    {
+        fichasTotales=ft;
+    }
+    
+    public int getJugadasGanadasBJ()
+    {
+        return jugadasGanadasBJ;
+    }
+    public void setJugadasGanadasBJ(int JGBJ)
+    {
+        jugadasGanadasBJ=JGBJ;
+    }
+    
+    public int getJugadasGanadas7()
+    {
+        return jugadasGanadas7;
+    }
+    public void setJugadasGanadas7(int JG7)
+    {
+        jugadasGanadas7=JG7;
+    }
+
+    public int getJugadasPerdidasBJ()
+    {
+        return jugadasPerdidasBJ;
+    }
+    public void setJugadasPerdidasBJ(int JPBJ)
+    {
+        jugadasPerdidasBJ=JPBJ;
+    }
+    
+    public int getJugadasPerdidas7()
+    {
+        return jugadasPerdidas7;
+    }
+    public void setJugadasPerdidas7(int JG7)
+    {
+        jugadasPerdidas7=JG7;
+    }
+    
+    public int getJugadasEmpatadas()
+    {
+        return jugadasEmpatadas;
+    }
+    public void setJugadasEmpatadas(int JE)
+    {
+        jugadasEmpatadas=JE;
+    }
+}
