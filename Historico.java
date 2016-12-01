1066a51f867420d87b6084b1355a386a63715720
JorgePrG97 <jorgeprg97@gmail.com>
2016-12-01 10:42:39 +0100

    medioHistorico

--- Historico.java
+++ Historico.java
@@ -2,5 +2,43 @@ package ludoteca;
 
 public class Historico
 {
+    
+    
+    public void resultados(Jugador J)
+    {
+        
+    }
+    
+    public void clasificacion()
+    {
+        
+    }
+    
+    public void buscarJugador(String nombre)
+    {
+        
+    }
+    
+    public void anadirJugador(Jugador J)
+    {
+        
+    }
+    
+    public void anadirPartida(Partida P)
+    {
+        
+    }
+    
+    public void listadoPartidas()
+    {
+        
+    }
+    
+    public void numJugadas(Partida P)
+    {
+        
+    }
+    
+    
 
 }
