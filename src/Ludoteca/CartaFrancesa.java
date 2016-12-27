7e84f663bb89c22bedb2e98f1bc67f8ce667b1a8
sito22 <sito_diaz@hotmail.es>
2016-12-27 10:54:08 +0100

    Update CartaFrancesa.java

--- src/Ludoteca/CartaFrancesa.java
+++ src/Ludoteca/CartaFrancesa.java
@@ -1,6 +1,22 @@
 package Ludoteca;
 
-public class CartaFrancesa
-{
+//La funcion ocultar no entiendo muy bien lo que hace, asi que nos tocara debatirlo
 
+public class CartaFrancesa extends Carta
+{
+  private enum palo{Picas, Diamantes, Corazones, Tréboles};
+    
+  public CartaFrancesa(int valor, enum palo)
+  {
+      this.valor = valor;
+      this.palo = palo;
+  }
+    
+  public void setValor(int valor)
+  {
+      this.valor = valor;
+  }
+    
+  public void ocultar();    
+    
 }
