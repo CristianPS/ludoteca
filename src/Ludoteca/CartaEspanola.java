8d4f0fd6af14b7055695676cca244bc3ddf63998
sito22 <sito_diaz@hotmail.es>
2016-12-27 10:54:56 +0100

    Update CartaEspanola.java

--- src/Ludoteca/CartaEspanola.java
+++ src/Ludoteca/CartaEspanola.java
@@ -2,5 +2,11 @@ package Ludoteca;
 
 public class CartaEspanola
 {
-
+  private enum palo{Oros, Bastos, Espadas, Copas};
+    
+  public CartaEspanola(int valor, enum palo)
+  {
+      this.valor = valor;
+      this.palo = palo;
+  }
 }
