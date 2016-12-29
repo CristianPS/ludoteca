f2f4b809a627241126dc98fde74c19bb13d7ecce
sito22 <sito_diaz@hotmail.es>
2016-12-29 15:32:14 +0100

    Update Jugada7ymedia.java

--- src/Ludoteca/Jugada7ymedia.java
+++ src/Ludoteca/Jugada7ymedia.java
@@ -5,4 +5,14 @@ public abstract class Jugada7ymedia extends Jugada
     public void jugarBanca(){
         
     }
+    public void jugarJugador(){
+    
+    }
+    public void añadirMano(m){
+    
+    }
+    
+    public Carta pedirCarta(){
+        
+    }
 }
