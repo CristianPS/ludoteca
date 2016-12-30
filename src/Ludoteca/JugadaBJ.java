f6f8f66f5725df9d54b7dd8940ade2f045b2ff5b
CristianPS <cristianvk97@gmail.com>
2016-12-30 11:12:01 +0100

    Cambios jugada, jugada7y1/2, jugadabj
    He medio hecho JugadaBJ, faltan cosas, y he hecho los cambios en
    Jugada7ymedia para que no tengamos definida dos veces el metodo
    peditCarta(). También creo que he puesto de una manera más compacta y
    más "Correcta" cuando se hacen las apuestas. Antes creo que aunque
    hubiera una apuesta incorrecta saldría: "Su apuesta es: "num"."

--- src/Ludoteca/JugadaBJ.java
+++ src/Ludoteca/JugadaBJ.java
@@ -1,6 +1,82 @@
 package Ludoteca;
 
-public class JugadaBJ
-{
+import java.util.ArrayList;
+import java.util.Scanner;
+        
 
+public abstract class JugadaBJ extends Jugada
+{
+    private Baraja baraja;
+    private Jugador jugador;
+    ArrayList<Integer> cartasSacadas = new ArrayList();
+    
+    
+    @Override
+    public void jugarBanca()
+    {
+    
+    }
+    
+    public void jugarJugador()
+    {
+        //Al inicio se apuesta una cantidad mayor o igual que 25
+        apostar();        
+        //Luego el jugador va pidiendo cartas hasta que no quiera mas
+        for(int i=0; i<40; i++)
+        {
+            pedirCarta(cartasSacadas);
+            System.out.println("¿Desea pedir mas cartas? S/N");
+            Scanner kbd = new Scanner(System.in);
+            String resp = kbd.nextLine();
+            if(resp.equals("S")) continue;
+            if(resp.equals("N")) break;
+        }
+    }
+    
+    public void añadirMano(Mano m)
+    {
+    
+    }
+    
+    public Carta pedirCarta(ArrayList cartasSacadas) //He hecho que le llegue como argumento el arrayList de las cartas sacadas, porque si no el array con las cartas sacadas se resetea cada vez que se pida una carta.
+    {
+        /*int numeroAleatorio = (int) (Math.random()*baraja.size()+0);
+        CartaEspanola c = (CartaEspanola) baraja.get(numeroAleatorio);
+        return c;*/
+        
+        int cs = 0;
+        int numeroAleatorio;
+        do
+        {
+            numeroAleatorio = (int) (Math.random()*baraja.tamaño()+0);
+            cartasSacadas.add(cs, numeroAleatorio);
+        }
+        while(cs != 0);
+        cartasSacadas.add(numeroAleatorio);
+        CartaEspanola c = (CartaEspanola) baraja.obtener(numeroAleatorio);
+        return c;
+    }
+    
+    @Override
+    public int apostar()
+    {
+        System.out.println("Por favor introduzca su apuesta. Le recordamos que la apuesta minima es de 10 fichas");
+        Scanner kbd = new Scanner(System.in);
+        int num = kbd.nextInt();
+        while(num<10 || num>jugador.getFichasTotales())
+        {
+            System.out.println("Lo sentimos, la apuesta no es correcta");
+            System.out.println("Por favor introduzca su apuesta. Le recordamos que la apuesta minima es de 10 fichas, y que esta no podra superar las fichas de las que disponga."); 
+            //Scanner kbd = new Scanner(System.in);
+            num = kbd.nextInt();               
+        }
+        System.out.println("Su apuesta es: " +num);
+        
+        return num;
+    }
 }
+    
+    
+    
+    
+
