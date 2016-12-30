6203b843797478b95c7819bce296be73432b1224
sito22 <sito_diaz@hotmail.es>
2016-12-30 10:08:59 +0100

    Update JugadaBJ.java

--- src/Ludoteca/JugadaBJ.java
+++ src/Ludoteca/JugadaBJ.java
@@ -1,6 +1,73 @@
 package Ludoteca;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class JugadaBJ
 {
+    private Baraja baraja;
+    private Jugador jugador;
+    private ArrayList<Integer> cartasSacadas = new ArrayList();
+    
+    @Override
+    public void jugarBanca()
+    {
+        
+    }
+    @Override
+    public void jugarJugador()
+    {
+        //Al inicio se apuesta una cantidad mayor o igual que 25
+        apostar();        
+        //Luego el jugador va pidiendo cartas hasta que no quiera mas
+        for(int i=0; i<40; i++)
+        {
+            pedirCarta();
+            System.out.println("¿Desea pedir mas cartas? S/N");
+            Scanner kbd = new Scanner(System.in);
+            String resp = kbd.nextLine();
+            if(resp.equals("S")) continue;
+            if(resp.equals("N")) break;
+        }
+    }
+    public void añadirMano(Mano m)
+    {
+    
+    }
+    
+    public Carta pedirCarta()
+    {
+        /*int numeroAleatorio = (int) (Math.random()*baraja.size()+0);
+        CartaEspanola c = (CartaEspanola) baraja.get(numeroAleatorio);
+        return c;*/       
+        int cs = 0;
+        int numeroAleatorio;
+        do
+        {
+            numeroAleatorio = (int) (Math.random()*baraja.tamaño()+0);
+            cartasSacadas.add(cs, numeroAleatorio);
+        }
+        while(cs != 0);
+        cartasSacadas.add(numeroAleatorio);
+        CartaFrancesa c = (CartaFrancesa) baraja.obtener(numeroAleatorio);
+        return c;
+    }
+    
+    public int apostar()
+    {
+        System.out.println("Por favor introduzca su apuesta. Le recordamos que la apuesta minima es de 10 fichas");
+        Scanner kbd = new Scanner(System.in);
+        int num = kbd.nextInt();
+        System.out.println("Su apuesta es: " +num);
+        do{
+           System.out.println("Lo sentimos, la apuesta no es correcta");
+           System.out.println("Por favor introduzca su apuesta. Le recordamos que la apuesta minima es de 10 fichas"); 
+           //Scanner kbd = new Scanner(System.in);
+           num = kbd.nextInt();
+           System.out.println("Su apuesta es: " +num);
+        }while(num<10 || num>jugador.getFichasTotales());
+        
+        return num;
+    }
 
 }
