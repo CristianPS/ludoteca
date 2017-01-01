617c5aad42ef05ab9b07aa2e1327f4767dfd06d2
CristianPS <cristianvk97@gmail.com>
2017-01-01 18:27:24 +0100

    Está explicado en el comentario, no puede haber 8, 9 o 10 en la baraja española.

--- src/Ludoteca/BarajaEspanola.java
+++ src/Ludoteca/BarajaEspanola.java
@@ -4,8 +4,18 @@ public class BarajaEspanola extends Baraja
 {
     public BarajaEspanola()
     {
+        //He cambiado esto porque en las siete y media no tiene que haber 8, 9 y 10. De momento lo he separado en dos bucles, seguro que hya una forma mejor de hacerlo pero de momento lo dejo asi.
+        for(int i=0; i<=7; i++)
+        {
+            for(int j=0; j<=4; i++)
+            {
+                
+                CartaEspanola c = new CartaEspanola(i, j);
+                baraja.add(c);
+            }
+        }
         
-        for(int i=0; i<=12; i++)
+        for(int i=10; i<=13; i++)
         {
             for(int j=0; j<=4; i++)
             {
