8d55170caaabe38312ddcb345650dea9d0e1c68e
JorgePrG97 <jorgeprg97@gmail.com>
2017-01-05 10:34:25 +0100

    Ventana

--- src/Ludoteca/PrimeraVentana.java
+++ src/Ludoteca/PrimeraVentana.java
@@ -34,6 +34,7 @@ public class PrimeraVentana extends javax.swing.JFrame {
         jRadioButton2 = new javax.swing.JRadioButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jRadioButtonMenuItem1 = new javax.swing.JRadioButtonMenuItem();
@@ -51,7 +52,7 @@ public class PrimeraVentana extends javax.swing.JFrame {
         jLabel1.setText("Bienvenido a la ludoteca CSJ");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 50, 530, -1));
 
-        jTextField1.setText("Introuzca su nombre....");
+        jTextField1.setText("Introduzca su nombre....");
         getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 330, 260, -1));
 
         jRadioButton1.setText("Jugar al Black Jack");
@@ -79,6 +80,14 @@ public class PrimeraVentana extends javax.swing.JFrame {
         jLabel4.setPreferredSize(new java.awt.Dimension(400, 300));
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 0, 560, 400));
 
+        jButton1.setText("Continuar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 390, -1, -1));
+
         jMenu1.setText("Accesos");
 
         jRadioButtonMenuItem1.setSelected(true);
@@ -102,6 +111,11 @@ public class PrimeraVentana extends javax.swing.JFrame {
     private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       VentanaJuego vJ = new VentanaJuego();
+       vJ.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -139,6 +153,7 @@ public class PrimeraVentana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
